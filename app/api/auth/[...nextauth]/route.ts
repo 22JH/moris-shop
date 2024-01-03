71a9ca5ac761a6a2cdf7cfe7190314bbdf66dac8
22/JH <dlwngud9303@naver.com>
2024-01-03 22:56:04 +0900

    [FE] FIX : authOptions 하나의 파일로 통합 및 export 삭제

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,51 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/app/lib/authOptions";
+import NextAuth, { AuthOptions } from "next-auth";
+import { connectToDB } from "@/app/lib/mongoose";
+import KakaoProvider from "next-auth/providers/kakao";
+import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
+import { createUser } from "@/app/lib/actions/userAction/signup.action";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID!,
+      clientSecret: process.env.NAVER_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, profile }: any) {
+      connectToDB();
+      await createUser(user);
+      return true;
+    },
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.user = {};
+        token.user.name = user?.name;
+        token.user.email = user?.email;
+        if (user.email === "toitoii080@nate.com" || user.role == "admin") {
+          token.user.role = "admin";
+        }
+      }
+      return token;
+    },
+    session: async ({ session, token }: any) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+  session: {
+    maxAge: 24 * 60 * 60, // 1일
+  },
+};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
